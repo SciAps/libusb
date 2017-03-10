11ba396f09b2530b48e800981906d75e50d9a923
paul <psoucy@sciaps.com>
2017-03-10 16:06:28 -0500

    fix build script

--- dist/Android.mk
+++ dist/Android.mk
@@ -1,7 +1,7 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
-LIBUSB_ROOT_REL:= ..
-LIBUSB_ROOT_ABS:= $(LOCAL_PATH)/..
+LIBUSB_ROOT_REL := ..
+LIBUSB_ROOT_ABS := $(LOCAL_PATH)/..
 
 include $(CLEAR_VARS)
 
@@ -17,11 +17,11 @@ LOCAL_C_INCLUDES += \
   $(LIBUSB_ROOT_ABS)/libusb \
   $(LIBUSB_ROOT_ABS)/libusb/os
 
-LOCAL_EXPORT_C_INCLUDE_DIRS := \
-  $(LIBUSB_ROOT_ABS)/libusb
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LIBUSB_ROOT_ABS)/libusb
 
 LOCAL_LDLIBS := -llog
 
 LOCAL_MODULE := libusb1.0
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
